7950ed4bd69e77166b319b3a99c7b9a580298075
lapoom <oyindolapoomolewa@gmail.com>
2025-11-21 12:45:31 +0100

    second commit

--- src/data/Skills.js
+++ src/data/Skills.js
@@ -1,21 +1,13 @@
 const skills = [
   "React.js",
-  "Next.js",
   "Tailwind CSS",
-  "Node.js",
-  "Express.js",
-  "MongoDB",
-  "Firebase",
   "JavaScript (ES6+)",
   "HTML5 & CSS3",
   "Git & GitHub",
   "Cybersecurity Fundamentals",
   "Ethical Hacking (CEH)",
-  "Networking (Cisco Certified)",
-  "Framer Motion",
-  "GSAP Animations",
   "Python",
-  "Linux & Bash",
+  "Linux",
 ];
 
 export default skills;
