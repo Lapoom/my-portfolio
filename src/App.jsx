7d72d7d2c375ec4814b6ab8997e09df37012453d
lapoom <oyindolapoomolewa@gmail.com>
2025-11-21 13:52:10 +0100

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Projects from "../pages/Projects.js";
+import Projects from "./pages/Projects";
 import ProjectDetails from "./pages/ProjectDetails";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
